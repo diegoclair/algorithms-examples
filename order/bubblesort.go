d1b77e937a0bc7a855b115623d981d89c50cde58
Diego Clair <diego93rodrigues@gmail.com>
2023-06-15 20:48:28 -0300

    feat: add buubleSort algorithm

---
+++ order/bubblesort.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+//example:  https://www.youtube.com/watch?v=8RkZoBZNNgI
+
+func main() {
+	BubbleSort(input)
+}
+
+//bubble sort is a order algorithm that compares two elements and swap them if the first one is bigger than the second one
+
+var input = []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
+
+// BubbleSort is a function that order a slice of integers
+func BubbleSort(input []int) {
+	fmt.Println("Unsorted:", input)
+	var changed bool
+	for i := 0; i < len(input)-1; i++ {
+		changed = false
+		for j := 0; j < len(input)-1; j++ {
+			if input[j] > input[j+1] {
+				input[j], input[j+1] = input[j+1], input[j]
+				changed = true
+			}
+		}
+		if !changed {
+			break
+		}
+	}
+	fmt.Println("Sorted:", input)
+}
